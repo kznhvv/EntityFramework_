1f7120181bab28f1915b8c7fcb9ce6ab70ffc94b
hvvkznc <havvakazanci61@hotmail.com>
2018-11-17 23:00:04 +0300

    Linq İşlemleri Bitti

--- NortwindEntity/NortwindEntity/Program.cs
+++ NortwindEntity/NortwindEntity/Program.cs
@@ -12,7 +12,7 @@ namespace NortwindEntity
     {
         static void Main(string[] args)
         {
-            OneMethod();
+            // OneMethod();
             //CustomerAdded();
             //CustomerOrdersAdd();
             //OrdersRemove();
@@ -21,17 +21,113 @@ namespace NortwindEntity
             //TumElemanlariListeleme();
             //SomeRowSelectList();
             //WhereMethod();
+            //GroupBy();
+            //Join1();
+            //Join2();
+            using (var nortwind = new NorthwindContext())
+            {
+                var result = nortwind.Customers.Where(c=>c.City=="London" && c.Country=="UK").
+                    OrderByDescending(c=>c.ContactName).
+                   // GroupBy(customer=>customer.Country).
+                    Select(cus=>new {cus.ContactName,cus.CustomerID });
+
+                //tembel yükleme
+                var result1 = nortwind.Customers.Include("Orders").Where(c=>c.CustomerID=="AROUT");
+                foreach (var customer in result1)
+                {
+                    Console.WriteLine("{0},{1}",customer.CustomerID,customer.Orders.Count);
+                }
+                foreach (var customer in result)
+                {
+                    //Console.WriteLine("{0},{1}",customer.CustomerID,customer.ContactName);
+                }
+            }
+
+
+
+                Console.ReadLine();
+        }
+
+        private static void Join2()
+        {
+            using (var nortwind = new NorthwindContext())
+            {
+
+                //bu şekilde hiç şiparişi olmayan müşterileri listeledik.
+                var result = from c in nortwind.Customers
+                             join o in nortwind.Orders
+                             on c.CustomerID equals o.CustomerID
+                             into temp
+                             from co in temp.DefaultIfEmpty()
+                                 // where temp.Count() == 0
+                             select new
+                             {
+                                 c.CustomerID,
+                                 c.ContactName,
+                                 c.CompanyName
+                             };
+
+
+                foreach (var customer in result)
+                {
+                    Console.WriteLine("Contact name:{0}, city:{1}, orderdate:{2}", customer.ContactName, customer.CompanyName, customer.CustomerID);
+                }
+                Console.WriteLine("{0} adet sipariş vardır.", result.Count());
+
+            }
+        }
+
+        private static void Join1()
+        {
             using (var nortwind = new NorthwindContext())
             {
                 ////c tüm kolonlar için kullanılır...
 
                 var result = from c in nortwind.Customers
-                                        group c by c.Country into g
-                                         select g;
+                             join o in nortwind.Orders
+                             on c.CustomerID equals o.CustomerID
+                             orderby c.CustomerID
+                             select new
+                             {
+                                 c.CustomerID,
+                                 c.ContactName,
+                                 o.OrderDate,
+                                 o.ShipCountry
+                             };
+
+                var result1 = from c in nortwind.Customers
+                              join o in nortwind.Orders
+                              on new { CustomerId = c.CustomerID, Sehir = c.City }
+                              equals new { CustomerId = o.CustomerID, Sehir = o.ShipCity }
+                              orderby c.CustomerID
+                              select new
+                              {
+                                  c.CustomerID,
+                                  c.ContactName,
+                                  o.OrderDate,
+                                  o.ShipCountry
+                             };
+                foreach (var customer in result)
+                {
+                    Console.WriteLine("Contact name:{0}, city:{1}, orderdate:{2}", customer.ContactName, customer.ShipCountry, customer.OrderDate);
+                }
+                Console.WriteLine("{0} adet sipariş vardır.", result.Count());
+
+            }
+        }
+        private static void GroupBy()
+        {
+            using (var nortwind = new NorthwindContext())
+            {
+                ////c tüm kolonlar için kullanılır...
+
+                var result = from c in nortwind.Customers
+                             group c by c.Country into g
+                             select g;
 
                 var result1 = from c in nortwind.Customers
                               group c by new { c.Country, c.City } into g
-                              select new { Sehir=g.Key.City, Ulke=g.Key.Country, Adet=g.Count()};
+                              select new { Sehir = g.Key.City, Ulke = g.Key.Country, Adet = g.Count() };
                 foreach (var customer in result)
                 {
                     //bu şekilde c.country i getirecek
@@ -44,9 +140,6 @@ namespace NortwindEntity
 
                 }
             }
-
-
-            Console.ReadLine();
         }
 
         private static void WhereMethod()
